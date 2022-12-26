42031053c468f96ad430dddc36a705c65814ce98
Aman <amanchopra.atg@gmail.com>
2022-12-26 23:26:52 +0530

    started with home page

---
+++ manage-my-project/src/components/Home/Home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {ArrowRight} from 'react-feather'
+import {useNavigate} from 'react-router-dom';
+import styles from './Home.module.css'
+// import homeScreen from '../../assets/homeScreen'
+
+const Home = () => {
+    const navigate = useNavigate();
+    const handleButton = () => {
+        navigate('/login');
+    };
+  return (
+    <div className={styles.container}>
+        <div className={styles.header}>
+            <div className={styles.left}>
+                <p className={styles.heading}>Personale Progetto Gestione</p>
+                <p className={styles.subHeading}>One Place Destination for your Project Management</p>
+                <button onClick={handleButton}>Get Started <ArrowRight /> </button>
+            </div>
+            <div className={styles.right}>
+                {/* <img src={homeScreen} alt='home' /> */}
+                <h1>Image</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
